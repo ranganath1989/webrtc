80aa2baf7a0aecb805f9fe32506a8ea78dd52138
ranganath1989 <vsr.ranganath@gmail.com>
2023-12-23 14:00:49 +0530

    poc_webrtc_second_commit

---
+++ frontend/js/utils.js
@@ -0,0 +1,248 @@
+'use strict';
+
+function openURL(url, blank = false) {
+    blank ? window.open(url, '_blank') : (window.location.href = url);
+}
+
+function IsSupportedWebRTC() {
+    let isWebRTCSupported = false;
+    ['RTCPeerConnection', 'webkitRTCPeerConnection', 'mozRTCPeerConnection', 'RTCIceGatherer'].forEach(function (item) {
+        if (isWebRTCSupported) return;
+        if (item in window) {
+            isWebRTCSupported = true;
+        }
+    });
+    return isWebRTCSupported;
+}
+
+function isMobile(userAgent) {
+    return !!/Android|webOS|iPhone|iPad|iPod|BB10|BlackBerry|IEMobile|Opera Mini|Mobile|mobile/i.test(userAgent || '');
+}
+
+function isTablet(userAgent) {
+    return /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(
+        userAgent,
+    );
+}
+
+function isIpad(userAgent) {
+    return /macintosh/.test(userAgent) && 'ontouchend' in document;
+}
+
+function isDesktop() {
+    return !isMobileDevice && !isTabletDevice && !isIPadDevice;
+}
+
+function logStreamSettingsInfo(name, stream) {
+    if (hasVideoTrack(stream)) {
+        console.log(name, {
+            video: {
+                label: stream.getVideoTracks()[0].label,
+                settings: stream.getVideoTracks()[0].getSettings(),
+            },
+        });
+    }
+    if (hasAudioTrack(stream)) {
+        console.log(name, {
+            audio: {
+                label: stream.getAudioTracks()[0].label,
+                settings: stream.getAudioTracks()[0].getSettings(),
+            },
+        });
+    }
+}
+
+function hasAudioTrack(mediaStream) {
+    if (!mediaStream) return false;
+    const audioTracks = mediaStream.getAudioTracks();
+    return audioTracks.length > 0;
+}
+
+function hasVideoTrack(mediaStream) {
+    if (!mediaStream) return false;
+    const videoTracks = mediaStream.getVideoTracks();
+    return videoTracks.length > 0;
+}
+
+function setTippy(element, content, placement) {
+    if (isMobileDevice) return;
+    if (element) {
+        if (element._tippy) {
+            element._tippy.destroy();
+        }
+        tippy(element, {
+            content: content,
+            placement: placement,
+        });
+    } else {
+        console.warn('setTippy element not found with content', content);
+    }
+}
+
+function changeAspectRatio(aspectRatio) {
+    const videoElements = document.querySelectorAll('video');
+    videoElements.forEach((video) => {
+        video.style.objectFit = aspectRatio ? 'contain' : 'cover';
+    });
+}
+
+function isFullScreen() {
+    const elementFullScreen =
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement ||
+        null;
+    if (elementFullScreen === null) return false;
+    return true;
+}
+
+function goInFullscreen(element) {
+    if (element.requestFullscreen) element.requestFullscreen();
+    else if (element.mozRequestFullScreen) element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+    else if (element.msRequestFullscreen) element.msRequestFullscreen();
+    else popupMessage('warning', 'Full screen', 'Full screen mode not supported by this browser on this device.');
+}
+
+function goOutFullscreen() {
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+}
+
+function copyRoom() {
+    const tmpInput = document.createElement('input');
+    document.body.appendChild(tmpInput);
+    tmpInput.style.display = 'none';
+    tmpInput.value = roomURL;
+    tmpInput.select();
+    tmpInput.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(tmpInput.value).then(() => {
+        console.log('Copied to clipboard Join Link ', roomURL);
+        document.body.removeChild(tmpInput);
+        popupMessage(
+            'clean',
+            'Room sharing',
+            `<div id="qrRoomContainer">
+                <canvas id="qrRoom"></canvas>
+            </div>
+            <br/>
+            <p style="color:rgb(8, 189, 89);">Join from your mobile device</p>
+            <p style="background:transparent; color:white; font-family: Arial, Helvetica, sans-serif;">No need for apps, simply capture the QR code with your mobile camera Or Invite someone else to join by sending them the following URL</p>
+            <p style="color:rgb(8, 189, 89);">${roomURL}</p>`,
+        );
+        makeRoomQR();
+    });
+}
+
+async function shareRoom() {
+    try {
+        await navigator.share({ url: roomURL });
+    } catch (err) {
+        console.error('[Error] navigator share', err);
+    }
+}
+
+function handleBodyEvents() {
+    checkElements();
+    document.body.onmousemove = () => {
+        if (buttonsBar.style.display == 'none' && waitingDivContainer.style.display == 'none') {
+            toggleClassElements('videoHeader', true);
+            elemDisplay(buttonsBar, true);
+            animateCSS(buttonsBar, 'fadeInUp');
+        }
+    };
+}
+
+function checkElements() {
+    if (buttonsBar.style.display != 'none') {
+        toggleClassElements('videoHeader', false);
+        animateCSS(buttonsBar, 'fadeOutDown').then((msg) => {
+            elemDisplay(buttonsBar, false);
+        });
+    }
+    setTimeout(checkElements, 20000);
+}
+
+function toggleClassElements(className, displayState) {
+    const elements = document.getElementsByClassName(className);
+    for (let i = 0; i < elements.length; i++) {
+        elemDisplay(elements[i], displayState);
+    }
+}
+
+function startSessionTime() {
+    let sessionElapsedTime = 0;
+    setInterval(function printTime() {
+        sessionElapsedTime++;
+        sessionTime.innerText = secondsToHms(sessionElapsedTime);
+    }, 1000);
+}
+
+function secondsToHms(d) {
+    d = Number(d);
+    let h = Math.floor(d / 3600);
+    let m = Math.floor((d % 3600) / 60);
+    let s = Math.floor((d % 3600) % 60);
+    let hDisplay = h > 0 ? h + 'h' : '';
+    let mDisplay = m > 0 ? m + 'm' : '';
+    let sDisplay = s > 0 ? s + 's' : '';
+    return hDisplay + ' ' + mDisplay + ' ' + sDisplay;
+}
+
+function getTime() {
+    const date = new Date();
+    return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+}
+
+function escapeSpecialChars(regex) {
+    return regex.replace(/([()[{*+.$^\\|?])/g, '\\$1');
+}
+
+function getLineBreaks(message) {
+    return (message.match(/\n/g) || []).length;
+}
+
+function elemDisplay(element, display, mode = 'block') {
+    element.style.display = display ? mode : 'none';
+}
+
+function elemDisable(element, disable) {
+    element.disabled = disable;
+}
+
+function animateCSS(element, animation, prefix = 'animate__') {
+    return new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        element.classList.add(`${prefix}animated`, animationName);
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            element.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+        element.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
+}
+
+function makeRoomQR() {
+    const qr = new QRious({
+        element: document.getElementById('qrRoom'),
+        value: roomURL,
+    });
+    qr.set({
+        size: 256,
+    });
+}
+
+async function playSound(name) {
+    const sound = '../sounds/' + name + '.mp3';
+    const audioToPlay = new Audio(sound);
+    try {
+        audioToPlay.volume = 0.5;
+        await audioToPlay.play();
+    } catch (err) {
+        return false;
+    }
+}
